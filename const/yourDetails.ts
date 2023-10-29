50312009c4aedab07815c247d005f197f01d8f16
matty33 <harden.construction@gmail.com>
2023-10-29 02:26:34 -0400

    Update yourDetails.ts

--- const/yourDetails.ts
+++ const/yourDetails.ts
@@ -1,8 +1,8 @@
-// Replace this with your contract address
+// Unlock NFT For Investors Qualified to Purchase From Detroit Housing Development Corp.
 export const contractAddress = "0x118967278f3B67bF73A25a1B2cdA8FcABcF991a7";
 
 // Replace this with your domain name.
-export const domainName = "thirdweb-example.com";
+export const domainName = "affordabe.homes.propy";
 
 // Minimum number of tokens required to access
 export const minimumBalance = 1;
