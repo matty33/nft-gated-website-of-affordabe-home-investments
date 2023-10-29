6bc0185fc7b862ada430895c0029ea7cfea10b45
matty33 <harden.construction@gmail.com>
2023-10-29 02:17:43 -0400

    Update yourDetails.ts

--- const/yourDetails.ts
+++ const/yourDetails.ts
@@ -1,5 +1,5 @@
 // Replace this with your contract address
-export const contractAddress = "0x436492DBc2E30E56FaC8F2297BD1964833c0687d";
+export const contractAddress = "0x118967278f3B67bF73A25a1B2cdA8FcABcF991a7";
 
 // Replace this with your domain name.
 export const domainName = "thirdweb-example.com";
@@ -7,5 +7,5 @@ export const domainName = "thirdweb-example.com";
 // Minimum number of tokens required to access
 export const minimumBalance = 1;
 
-// Token ID for ERC-1155 tokens
-export const erc1155TokenId = 0;
+// Token ID for ERC-721 tokens
+export const erc721TokenId = 0;
